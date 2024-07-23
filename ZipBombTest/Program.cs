a6835984b9eead7358a53cbc38d241ff52163202
Pavel <shmachilin@gmail.com>
2024-07-23 19:02:18 +0000

    Удаляем хлам

--- ZipBombTest/Program.cs
+++ ZipBombTest/Program.cs
@@ -2,23 +2,7 @@
 
 Console.Clear();
 
-var dest = new MemoryStream(100);
 
-using (var src = Enumerable.Range(0, 100).Select(i => (byte)i).ToStream())
-{
-    src.CopyTo(dest);
-}
-
-var result = dest.ToArray();
-
-for (var i = 0; i < result.Length; i++)
-{
-    Console.Write("{0,2},", result[i]);
-    if ((i + 1) % 20 == 0)
-        Console.WriteLine();
-}
-
-Console.WriteLine();
 Console.WriteLine("End.");
 
 return;
