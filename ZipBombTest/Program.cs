3817c4dfb20db713c49775c8a513ef78f7132f52
Pavel <shmachilin@gmail.com>
2024-07-23 18:46:28 +0000

    Настройка сочетаний клавишь

--- ZipBombTest/Program.cs
+++ ZipBombTest/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Clear();
+﻿using ZipBombTest.IO;
+
+Console.Clear();
+
+using (var src = new EnumToStream(Enumerable.Range(0, 100).Select(i => (byte)i)))
+{
+
+}
 
 Console.WriteLine("End.");
 
